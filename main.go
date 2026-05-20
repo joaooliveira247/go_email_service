1bb3ec60368cf357621451bf5cb468e45f8c99f4
joaooliveira247 <joaooliveira247@gmail.com>
2026-05-20 10:54:52 -0300

    feat(main): add main func

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/joaooliveira247/go_email_service/src/config"
+import (
+	"fmt"
+
+	"github.com/joaooliveira247/go_email_service/src/config"
+)
 
 func init() {
 	config.LoadEnv()
+}
+
+func main() {
+	fmt.Println("Hello Go email service")
 }
