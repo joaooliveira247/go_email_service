a5fdbed11f58a043cba610ddf6fb173ba8f1d640
joaooliveira247 <joaooliveira247@gmail.com>
2026-05-20 10:40:46 -0300

    feat(config): add LoadEnv func to load .env file in OS env vars

--- src/config/config.go
+++ src/config/config.go
@@ -1,3 +1,17 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 var RabbitURL = ""
+
+func LoadEnv() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatalf("(Config/dotenv): %s\n", err)
+	}
+}
