df32fb485fc5bf583427dfd82e067a4ecac8ae33
joaooliveira247 <joaooliveira247@gmail.com>
2026-07-11 15:48:26 -0300

    feat(main): add slog config in init function

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/joaooliveira247/go_email_service/src/config"
 )
 
 func init() {
+	handler := slog.NewJSONHandler(
+		os.Stdout,
+		&slog.HandlerOptions{Level: slog.LevelInfo, AddSource: true},
+	)
+
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
 	config.LoadEnv()
 }
 
 func main() {
+	slog.Info("Go email service started!")
 	fmt.Println("Hello Go email service")
 }
