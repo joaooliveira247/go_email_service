c17d0166f4633b2193f1b7dde335822e6549cb90
joaooliveira247 <joaooliveira247@gmail.com>
2026-07-11 11:35:58 -0300

    fix(config): fix .env var name from os.Getenv

--- src/config/config.go
+++ src/config/config.go
@@ -19,6 +19,6 @@ func LoadEnv() {
 
 	RabbitURL = fmt.Sprintf("amqp://%s:%s@%s:%s/",
 		os.Getenv("RABBIT_USER"), os.Getenv("RABBIT_PASSWORD"),
-		os.Getenv("RABIIT_HOST"), os.Getenv("RABBIT_PORT"),
+		os.Getenv("RABBIT_HOST"), os.Getenv("RABBIT_PORT"),
 	)
 }
