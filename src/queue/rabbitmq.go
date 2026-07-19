c9792f66e5c52a1203f8c26df29efc103099a6c7
joaooliveira247 <joaooliveira247@gmail.com>
2026-07-19 20:54:17 -0300

    feat(queue): add declareQueues in RabbitConnection struct to create all queues

--- src/queue/rabbitmq.go
+++ src/queue/rabbitmq.go
@@ -1,6 +1,9 @@
 package queue
 
 import (
+	"fmt"
+
+	"github.com/joaooliveira247/go_email_service/src/config"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -9,4 +12,55 @@ type RabbitConnection struct {
 	Channel *amqp.Channel
 }
 
+func (rc *RabbitConnection) declareQueues() error {
 
+	// DLQ declaration
+	_, err := rc.Channel.QueueDeclare(
+		config.EmailDLQQueue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return fmt.Errorf("Failed when try declare DLQ: %w", err)
+	}
+
+	// Retry Queue
+	retryArgs := amqp.Table{
+		"x-dead-letter-exchange":    config.RabbitExchange,
+		"x-dead-letter-routing-key": config.EmailQueue,
+		"x-message-ttl":             config.EmailRetryTTL,
+	}
+
+	_, err = rc.Channel.QueueDeclare(
+		config.EmailRetryQueue,
+		true,
+		false,
+		false,
+		false,
+		retryArgs,
+	)
+
+	if err != nil {
+		return fmt.Errorf("Failed when try declare RetryQueue: %w", err)
+	}
+
+	// Main Queue
+	_, err = rc.Channel.QueueDeclare(
+		config.EmailQueue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return fmt.Errorf("Failed when try declare MainQueue: %w", err)
+	}
+
+	return nil
+}
